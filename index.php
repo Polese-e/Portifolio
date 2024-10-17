df7077d1c7a96f82466878d17ceac69002d89874
Polese <enzopolesedamasceno@gmail.com>
2024-10-17 14:44:18 -0300

    Estilizando a main

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Portifólio</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-900 text-gray-200">
+    <header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
+        <div class="font-bold text-xl font-mono text-cyan-600">
+            Meu Portifólio
+        </div>
+        
+        <div>
+            <ul class="flex gap-x-3 font-medium">
+                <a href="" class="hover:underline"><li>Projetos</li></a>
+                <a href="" class="hover:underline"><li>Github</li></a>
+                <a href="" class="hover:underline"><li>Linkedin</li></a>
+                <a href="" class="hover:underline"><li>Twitter</li></a>
+            </ul>
+        </div>
+    </header> 
+    
+    <main class="mx-auto max-w-screen-lg min-h-20">
+        <section class="flex">
+            <div class="w-2/3">
+                <h1 class="text-3xl font-bold">Oi, meu nome é Enzo!</h1>
+                <p class="text-xl leading-6 mt-6">Falando algo sobre mim blabalblalblbalalblalb abla blal bal bla blalb alb all</p>
+                <ul class="flex gap-x-3 mt-3">
+                    <li"><a >X</a></li>
+                    <li"><a href="">Instagram</a></li>
+                    <li"><a href="">Discord</a></li>
+                    <li"><a href="">X</a></li>
+                </ul>
+            </div>
+
+            <div class="w-1/3 flex items-center justify-center">
+                <div>foto</div>
+            </div>
+        </section>
+    </main>
+    </body>
+</html>
