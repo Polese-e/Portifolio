e44477d4e5387e87e1fcd13d3b7fd665e87f57a5
Polese <enzopolesedamasceno@gmail.com>
2024-10-21 15:25:10 -0300

    Deixando o hero dinâmico

--- components/hero.php
+++ components/hero.php
@@ -1,3 +1,12 @@
+<?php 
+    $itens = [
+        ['href' => 'https://www.linkedin.com/in/polese-e/', 'src' => 'assets/linkedin.png', 'Alt' => 'Linkedin Logo'],
+        ['href' => 'https://github.com/Polese-e', 'src' => 'assets/github.png', 'Alt' => 'Github Logo'],
+        ['href' => 'https://wa.me/qr/PX244DM2FE7IG1', 'src' => 'assets/whatsapp.png', 'Alt' => 'WhatsApp Logo'],
+        ['href' => 'https://www.instagram.com/polese_e/', 'src' => 'assets/instagram.png', 'Alt' => 'Instagram Logo'],
+    ]
+?>
+
 <section class="flex flex-col-reverse md:flex-row items-center gap-x-10">
     <div class="w-full md:w-2/3 my-12" data-aos="fade-right">
         <h1 class="text-4xl font-bold mb-4">Olá, meu nome é Enzo!</h1>
@@ -6,30 +15,14 @@
             criativa, sempre buscando novos desafios e oportunidades para crescer profissionalmente.
         </p>
         <ul class="flex gap-x-6 mt-4">
+            <?php foreach($itens as $item): ?>
             <li>
-                <a href="https://www.linkedin.com/in/polese-e/" target="_blank">
-                    <img src="assets/linkedin.png" alt="LinkedIn Logo"
+                <a href="<?=$item['href']?>" target="_blank">
+                    <img src="<?=$item['src']?>" alt="<?=$item['Alt']?>"
                         class="h-8 hover:scale-125 transition-transform duration-300" />
                 </a>
             </li>
-            <li>
-                <a href="https://www.instagram.com/polese_e/" target="_blank">
-                    <img src="assets/instagram.png" alt="Instagram Logo"
-                        class="h-8 hover:scale-125 transition-transform duration-300" />
-                </a>
-            </li>
-            <li>
-                <a href="https://github.com/Polese-e" target="_blank">
-                    <img src="assets/github.png" alt="GitHub Logo"
-                        class="h-8 hover:scale-125 transition-transform duration-300" />
-                </a>
-            </li>
-            <li>
-                <a href="https://wa.me/qr/PX244DM2FE7IG1" target="_blank">
-                    <img src="assets/whatsapp.png" alt="Discord Logo"
-                        class="h-8 hover:scale-125 transition-transform duration-300" />
-                </a>
-            </li>
+            <?php endforeach; ?>
         </ul>
     </div>
     <div class="w-full md:w-1/3 flex items-center justify-center mb-12 md:mb-0" data-aos="fade-left">
